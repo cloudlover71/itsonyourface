4700f8859843fe01c6c7d5df8ff1cc55e0b50022
Trofim Prodayvoda <t.prodayvoda@gmail.com>
2016-06-09 15:43:42 +0300

    Init project structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+  name='emf_sdk',
+  version='0.1',
+  description='Expedia EMF SDK',
+  package_dir={'emf_sdk': 'emf_sdk'},
+  packages=['emf_sdk'],
+  install_requires=['fluent-logger'],
+  author='Trofim Prodayvoda',
+  author_email='t.prodayvoda@gmail.com',
+  url='https://github.com/radiusinc/Expedia-EMF-SDK',
+  download_url='',
+  license=''
+)
