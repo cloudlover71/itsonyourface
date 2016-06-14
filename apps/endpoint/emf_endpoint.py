c25c75f462276f3a274aead55fcaaf4439dcb1b2
Trofim Prodayvoda <t.prodayvoda@gmail.com>
2016-06-14 18:02:11 +0300

    TLS server/client proof of concept

--- apps/endpoint/emf_endpoint.py
+++ apps/endpoint/emf_endpoint.py
@@ -1,4 +1,9 @@
+import ssl
 import asyncio
+
+
+sslcontext = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+sslcontext.load_cert_chain(certfile="ssl_keys/server.crt", keyfile="ssl_keys/server.key")
 
 
 @asyncio.coroutine
@@ -7,7 +12,7 @@ def handle_connection(_, writer):
 
 loop = asyncio.get_event_loop()
 
-server_gen = asyncio.start_server(handle_connection, port=8004)
+server_gen = asyncio.start_server(handle_connection, port=8004, ssl=sslcontext)
 server = loop.run_until_complete(server_gen)
 
 loop.run_forever()
--- apps/latency_monitor/emf_latency_monitor.py
+++ apps/latency_monitor/emf_latency_monitor.py
@@ -1,73 +1,41 @@
+import ssl
 import time
-import asyncio
-import urllib
-
+import socket
+from urllib.parse import urlsplit
 
 from emf_sdk import sender_factory, SENDER_TYPE
 
 
-class EchoClientProtocol(asyncio.Protocol):
-    def __init__(self, loop):
-        self.loop = loop
-        print(time.time())
+url = urlsplit('//localhost')
+query = ('HEAD {path} HTTP/1.0\r\n'
+         'Host: {hostname}\r\n'
+         '\r\n').format(path=url.path or '/', hostname=url.hostname)
 
-    def connection_made(self, transport):
-        print('Connection made')
-        print(time.time())
-        transport.close()
+print("Query:", query)
 
-    def data_received(self, data):
-        print('Data received: {!r}'.format(data.decode()))
+while True:
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        sock.setblocking(True)
+        sock.settimeout(3)
+        sock = ssl.wrap_socket(sock, cert_reqs=ssl.CERT_NONE)
+        sock.connect((url.hostname, 8004))
+        print('Connected')
 
-    def connection_lost(self, exc):
-        print('The server closed the connection')
-        print('Stop the event loop')
-        self.loop.stop()
+        sock.sendall(query.encode())
+        data = sock.recv(1024).decode().split()
+        print('Data received: {!r}'.format(data))
 
-loop = asyncio.get_event_loop()
-coro = loop.create_connection(lambda: EchoClientProtocol(loop), '127.0.0.1', 8004)
-loop.run_until_complete(coro)
-loop.run_forever()
-loop.close()
+        #print("Ping: ", "OK" if data[1] == '200' else "-1")
 
-
-@asyncio.coroutine
-def print_http_headers(url):
-    url = urllib.parse.urlsplit(url)
-    if url.scheme == 'https':
-        connect = asyncio.open_connection(url.hostname, 443, ssl=True)
-    else:
-        connect = asyncio.open_connection(url.hostname, 80)
-    reader, writer = yield from connect
-    query = ('HEAD {path} HTTP/1.0\r\n'
-             'Host: {hostname}\r\n'
-             '\r\n').format(path=url.path or '/', hostname=url.hostname)
-    writer.write(query.encode('latin-1'))
-    while True:
-        line = yield from reader.readline()
-        if not line:
-            break
-        line = line.decode('latin1').rstrip()
-        if line:
-            print('HTTP header> %s' % line)
-
-    # Ignore the body, close the socket
-    writer.close()
-
-loop = asyncio.get_event_loop()
-task = asyncio.ensure_future(print_http_headers('127.0.0.1'))
-loop.run_until_complete(task)
-loop.close()
-
-
-params = {
-    'tag': 'emf.debug',
-    'host': 'localhost',
-    'port': 24224,
-}
-
-sender = sender_factory(SENDER_TYPE.FLUENT, params)
-sender.send('test_label', {
-    'key1': 'val1',
-    'key2': 'val2'
-})
+        time.sleep(3)
+    except socket.timeout:
+        print('Timeout')
+    except ConnectionError:
+        pass
+    except KeyboardInterrupt:
+        print('Stopped')
+        break
+    finally:
+        print('Socket closed')
+        sock.close()
