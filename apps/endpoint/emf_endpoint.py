7ec64a963d6783304e1c289dbe6061ea0e1ff60f
Trofim Prodayvoda <t.prodayvoda@gmail.com>
2016-06-20 11:42:26 +0300

    Endpoint command line args

--- apps/endpoint/emf_endpoint.py
+++ apps/endpoint/emf_endpoint.py
@@ -1,19 +1,48 @@
 import ssl
 import asyncio
+import argparse
 
 
-sslcontext = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
-sslcontext.load_cert_chain(certfile="ssl_keys/server.crt", keyfile="ssl_keys/server.key")
+parser = argparse.ArgumentParser(description='EMF SDK Endpoint')
+parser.add_argument('--host', type=str, required=False)
+parser.add_argument('--port', type=int, required=True)
+parser.add_argument('--ssl-enabled', type=int, required=False, default=0, choices=[1, 0])
 
 
 @asyncio.coroutine
 def handle_connection(_, writer):
     writer.write('OK'.encode())
 
-loop = asyncio.get_event_loop()
 
-server_gen = asyncio.start_server(handle_connection, port=8004, ssl=sslcontext)
-server = loop.run_until_complete(server_gen)
+if __name__ == '__main__':
+    server_args = {}
+    endpoint_args = parser.parse_args()
 
-loop.run_forever()
-loop.close()
+    try:
+        if endpoint_args.ssl_enabled:
+            ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+            ssl_context.load_cert_chain(certfile="ssl_keys/server.crt", keyfile="ssl_keys/server.key")
+            server_args['ssl'] = ssl_context
+        server_args['port'] = endpoint_args.port
+        if endpoint_args.host:
+            server_args['host'] = endpoint_args.host
+
+    except FileNotFoundError:
+        print('SSL keys is missing')
+        exit()
+    except Exception as e:
+        print(e)
+        exit()
+
+    loop = asyncio.get_event_loop()
+
+    try:
+        server_gen = asyncio.start_server(handle_connection, **server_args)
+        server = loop.run_until_complete(server_gen)
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print('Stopped')
+    except Exception as e:
+        print(e)
+    finally:
+        loop.close()
